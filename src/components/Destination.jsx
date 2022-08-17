364ab131a17760af12c3ce53a891f9f588848d65
ajaxbo360 <ajakak.2016@gmail.com>
2022-08-17 22:18:50 +0100

    fix responsivness

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -13,7 +13,7 @@ function Destination() {
           <h1>All-Inclusive Resorts</h1>
           <p className='py-4'>On the Caribbian's Best Beaches</p>
 
-          <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4 '>
+          <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-4  md:gap-4 '>
               <img className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2 cursor-pointer rounded-md transition-transform hover:scale-105' src={BoraBora} alt="/" />
               <img className='w-full h-full object-cover cursor-pointer rounded-md transition-transform hover:scale-105' src={BoraBora2} alt="/" />
               <img className='w-full h-full object-cover cursor-pointer rounded-md transition-transform hover:scale-105' src={Maldives} alt="/" />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,12 +9,12 @@ const Hero = () => {
           <div className='absolute w-full h-full bg-gray-900/30 top-0 left-0'></div>
           <div className='w-full h-full top-0 absolute flex flex-col items-center justify-center text-white p-4'>
               <h1 className='mb-3'>First Class Travel</h1>
-              <h2>Top 1% Locations Worldwide</h2>
+              <h2 className='text-center'>Top 1% Locations Worldwide</h2>
               <form className='my-8 flex justify-between max-w-[700px] w-full border p-1 mx-auto rounded-md bg-gray-100/90'>
                   <input type="text" placeholder='Search Distinations' className='bg-transparent w-[300px] sm:w-[400px] focus:outline-none font-[Poppins]' />
                   
                   <div>
-                      <button className='hover:-translate-x-1 transition-all'><AiOutlineSearch size={20} className="icon " style={{color:"#fff"}}/></button>
+                      <button className='hover:-translate-x-1 transition-all -mx-11'><AiOutlineSearch size={20} className="icon " style={{color:"#fff"}}/></button>
                   </div>
               </form>
           </div>
